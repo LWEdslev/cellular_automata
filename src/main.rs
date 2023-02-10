f5b35a2aa46029c5e10a05cc78c3dd4bbc340b4f
Lasse Edslev <202010638@post.au.dk>
2023-02-10 16:24:23 +0100

    Added enviroment variables for - FPS - Window size - Number of cells per side in grid

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,39 @@ mod utility;
 
 use rand::Rng;
 use crate::automata::Automata;
+use std::env;
+use crate::utility::{DEFAULT_FPS, DEFAULT_GRID_SIZE, DEFAULT_WINDOW_SIZE};
 
 fn main() {
 
-    let size = 200;
+    let args: Vec<String> = env::args().collect();
 
-    let mut automata = Automata::new(size);
+    let get_value = |search_param: &str, default: u64| {
+        match args.iter().find(|s| s.to_lowercase().contains(&format!("{}=", search_param))) {
+            Some(s) => s.split('=').last().unwrap().parse().expect(&format!("There must be a number after {}=", search_param)),
+            None => default,
+    }};
+
+
+
+    let fps = get_value("fps", DEFAULT_FPS);
+
+    let grid_size = get_value("grid", DEFAULT_GRID_SIZE) as usize;
+
+    let window_size = get_value("window", DEFAULT_WINDOW_SIZE) as u32;
+
+
+    let mut automata = Automata::new(grid_size);
 
     let mut rng = rand::thread_rng();
 
-    for x in 0..size {
-        for y in 0..size {
+    for x in 0..grid_size {
+        for y in 0..grid_size {
             if rng.gen::<f64>() < 0.3 {
                 automata.birth_cell_at(x, y);
             }
         }
     }
 
-    gui::run(automata, 800, 20);
+    gui::run(automata, window_size, fps);
 }
--- src/utility.rs
+++ src/utility.rs
@@ -1,5 +1,10 @@
 use graphics::types::{Rectangle, Color};
 
+pub const DEFAULT_FPS: u64 = 60;
+pub const DEFAULT_WINDOW_SIZE: u64 = 500;
+pub const DEFAULT_GRID_SIZE: u64 = 100;
+
+
 pub trait Drawable {
     fn update(&mut self);
     fn get_new_graphics(&mut self, width: f64, height: f64) -> Vec<(Rectangle, Color)>;
